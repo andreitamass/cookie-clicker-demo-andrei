8c394dcace583f9c02ad29568972002c6420790c
andreitamass <andrei.tamass83@gmail.com>
2026-09-22 18:40:59 +0200

    imported swing

--- StartGame.java
+++ StartGame.java
@@ -1,6 +1,10 @@
+import javax.swing.JOptionPane;
+
 public class StartGame {
 
     public static void main(String[] args) {
+        
+        String name = JOptionPane.showInputDialog("whats your name?");
         
     }
 
