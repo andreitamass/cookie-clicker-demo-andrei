6d3f77fc04e218eec39b750ed7f1e9f95b00e1ef
andreitamass <andrei.tamass83@gmail.com>
2026-09-23 20:25:20 +0200

    added frame and button, also actionlistener to count clicks to be implemented

--- src/StartGame.java
+++ src/StartGame.java
@@ -1,31 +1,45 @@
 package src;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class StartGame extends JFrame {
 
+    //Constructor
     public StartGame() {
         setTitle("Cookie Window");
         setSize(500, 500);
 
+        JPanel Panel = new JPanel();
+
         JButton cookieButton = new JButton("Click!");
-        JPanel cookiePanel = new JPanel();
 
-        cookiePanel.add(cookieButton);
+        Label lb = new Label();
 
-        this.getContentPane().add(cookiePanel);
+        Panel.add(cookieButton);
+
+        add(Panel, BorderLayout.SOUTH);
 
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        cookieButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
         
-
     }
-
-
+    
     public static void main(String[] args) {
-        StartGame start = new StartGame();
+        //Starts the game
+        new StartGame();
     }
 
 }
